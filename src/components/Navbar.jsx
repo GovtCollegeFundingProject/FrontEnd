46033a56c3560ff45e763daa5185bb431b47365d
Eatha Surya Prajyesh <121669038+suryatenz@users.noreply.github.com>
2024-08-21 12:03:41 +0530

    new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-     <div className=" bg-white h-24 w-full m-6">
+     <div className=" flex flex-row bg-white h-24 w-full justify-between">
       <div className=" m-3">
-        <img src="/AA_hub_Logo.png" alt="logo" className="h-16 w-16" />
+        <img src="./AAhublogo.png" alt="logo" className=" h-28 w-28" />
       </div>
-      <div className=" flex justify-center">
-        <div className="pt-2">
+      <div className="flex flex-row">
+        <div className="">
           <a href="/" className="text-2xl font-semibold text-black pr-9">Home</a>
+          </div>
+          <div className="">  
           <a href="/about" className="text-2xl font-semibold text-black">About</a>
-        </div>
+          </div>
       </div>
+      <div className=""></div>
     </div> 
     </>
   )

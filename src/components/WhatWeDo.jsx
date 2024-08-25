31527d21ce191e1430123efa72853d6e7ebc9fd7
Eatha Surya Prajyesh <121669038+suryatenz@users.noreply.github.com>
2024-08-25 20:00:01 +0530

    chore: Update images in AboutUs and WhatWeDo components

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -7,7 +7,7 @@ const WhatWeDo = () => {
       <h2 className="text-center text-[#06038D] font-semibold text-3xl mb-8">What we do.</h2>
       <div className="flex justify-center space-x-48 mt-14">
         <div className="border-4 border-[#06038D] p-6 rounded-md w-1/4">
-          <img src={`https://via.placeholder.com/100`} alt="General Specific" className="mx-auto mb-4" />
+          <img src={`./image15.png`} alt="General Specific" className="mx-auto mb-4" />
           <h3 className="text-center text-xl font-semibold mb-2">General Specific</h3>
           <p className="text-center text-lg mb-4">
             Contribute to the Development of Infrastructure in Junior Colleges of Andhra Pradesh
@@ -17,7 +17,7 @@ const WhatWeDo = () => {
           </div>
         </div>
         <div className="border-4 border-[#06038D] p-6 rounded-md w-1/4">
-          <img src={`https://via.placeholder.com/100`} alt="College Specific" className="mx-auto mb-4" />
+          <img src={`./image116.png`} alt="College Specific" className="mx-auto mb-4" />
           <h3 className="text-center text-xl font-semibold mb-2">College Specific</h3>
           <p className="text-center text-lg mb-4">
             Contribute to the Development of Infrastructure in a specific Junior College of Andhra Pradesh
